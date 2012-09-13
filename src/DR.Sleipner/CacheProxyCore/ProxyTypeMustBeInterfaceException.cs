4fa9078b05332d1ec8db9f6fd6c7b5aff512a8eb
Jonas Swiatek <jals@dr.dk>
2012-09-13 15:48:35 +0200

    Draft code

---
+++ src/DR.Sleipner/CacheProxyCore/ProxyTypeMustBeInterfaceException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DR.Sleipner.CacheProxyCore
+{
+    public class ProxyTypeMustBeInterfaceException : Exception
+    {
+    }
+}
