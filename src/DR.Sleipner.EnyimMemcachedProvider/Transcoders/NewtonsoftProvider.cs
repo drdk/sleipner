d1d0ac5683d1f2a608350ea521d01a544a76966d
Jonas Swiatek <jals@dr.dk>
2012-09-28 15:05:08 +0200

    Added newtonsoft transcoder

---
+++ src/DR.Sleipner.EnyimMemcachedProvider/Transcoders/NewtonsoftProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Enyim.Caching.Memcached;
+
+namespace DR.Sleipner.EnyimMemcachedProvider.Transcoders
+{
+    public class NewtonsoftProvider : IProviderFactory<ITranscoder>
+    {
+        public ITranscoder Create()
+        {
+            return new NewtonsoftTranscoder();
+        }
+
+        public void Initialize(Dictionary<string, string> parameters)
+        {
+            //I have no idea what this does.
+        }
+    }
+}
---
+++ src/DR.Sleipner.EnyimMemcachedProvider/Transcoders/NewtonsoftTranscoder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Enyim.Caching.Memcached;
+using Newtonsoft.Json;
+
+namespace DR.Sleipner.EnyimMemcachedProvider.Transcoders
+{
+    public class NewtonsoftTranscoder : ITranscoder 
+    {
+        public CacheItem Serialize(object value)
+        {
+            var serializer = new JsonSerializer
+            {
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(memoryStream))
+                {
+                    serializer.Serialize(writer, value);
+                }
+                return new CacheItem()
+                {
+                    Data = new ArraySegment<byte>(memoryStream.ToArray()),
+                };
+            }
+        }
+
+        public object Deserialize(CacheItem item)
+        {
+            var jsonSerializer = new JsonSerializer
+            {
+                TypeNameHandling = TypeNameHandling.Objects
+            };
+
+            using (var memoryStream = new MemoryStream(item.Data.Array))
+            {
+                using (var streamReader = new StreamReader(memoryStream))
+                {
+                    var textReader = new JsonTextReader(streamReader);
+                    var dynamicEntity = jsonSerializer.Deserialize(textReader);
+
+                    return dynamicEntity;
+                }
+            }
+        }
+    }
+}
