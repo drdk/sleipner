0d9481f82d2cedd834227b2df443d39a65a237c0
Jonas Swiatek <jals@dr.dk>
2013-01-02 13:55:28 +0100

    Added documentation

--- src/DR.Sleipner/Config/IMethodFamilyConfigurationExpression.cs
+++ src/DR.Sleipner/Config/IMethodFamilyConfigurationExpression.cs
@@ -7,10 +7,39 @@ namespace DR.Sleipner.Config
 {
     public interface IMethodFamilyConfigurationExpression
     {
+        /// <summary>
+        /// Configures policy to cache the methods output for duration seconds
+        /// The default is zero (disabled)
+        /// </summary>
+        /// <param name="duration"></param>
+        /// <returns></returns>
         IMethodFamilyConfigurationExpression CacheFor(int duration);
+
+        /// <summary>
+        /// The maximum age the object can have in cache before it is considered unusable. Must be larger than duration specified in CacheFor
+        /// </summary>
+        /// <param name="maxDuration"></param>
+        /// <returns></returns>
         IMethodFamilyConfigurationExpression ExpireAfter(int maxDuration);
+
+        /// <summary>
+        /// Disables cache for this policy.
+        /// This is the default setting.
+        /// </summary>
+        /// <returns></returns>
         void DisableCache();
+
+        /// <summary>
+        /// If the real instance throws and exception, and there is a stale object already available, enabling this will supress the exception and renew the item in cache.
+        /// This is the default behavior
+        /// </summary>
+        /// <returns></returns>
         IMethodFamilyConfigurationExpression SupressExceptionsWhenStale();
+
+        /// <summary>
+        /// Bubbles exceptions from real instance into cache and to the caller.
+        /// </summary>
+        /// <returns></returns>
         IMethodFamilyConfigurationExpression BubbleExceptionsWhenStale();
     }
 }
