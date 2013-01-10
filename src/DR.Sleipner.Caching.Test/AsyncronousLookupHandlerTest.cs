7fdb008a0147f3db5745cb51c5c461d1176cd6b6
Jonas Swiatek <jals@dr.dk>
2013-01-10 15:43:27 +0100

    Full coverage of AsyncLookupHandler

--- src/DR.Sleipner.Caching.Test/AsyncronousLookupHandlerTest.cs
+++ src/DR.Sleipner.Caching.Test/AsyncronousLookupHandlerTest.cs
@@ -37,10 +37,29 @@ namespace DR.Sleipner.Caching.Test
         }
 
         [Test]
+        [ExpectedException(typeof(TestException))]
+        public void HandleTest_Fresh_Exception()
+        {
+            var testInterface = new Mock<ITestInterface>(MockBehavior.Strict);
+            var cacheProvider = new Mock<ICacheProvider<ITestInterface>>(MockBehavior.Strict);
+            var policyProvider = new Mock<IPolicyProvider<ITestInterface>>(MockBehavior.Strict);
+
+            var exception = new TestException();
+            var cachedObject = new CachedObject<string>(CachedObjectState.Fresh, exception);
+            var call = ExpressionHelpers.GetMethodCallDescription<ITestInterface, string>(a => a.ParameterLessMethod());
+
+            var lookupContext = new DefaultLookupContextBuilder<ITestInterface>().CreateLookupContext<string>(testInterface.Object, cacheProvider.Object, policyProvider.Object, call.Method, call.Parameters);
+            lookupContext.CurrentCachedObject = cachedObject;
+
+            var asyncHandler = new AsyncronousLookupHandler<ITestInterface>();
+            asyncHandler.Handle(lookupContext);
+        }
+
+        [Test]
         public void HandleTest_Stale()
         {
             var testInterface = new Mock<ITestInterface>(MockBehavior.Strict);
-            var cacheProvider = new Mock<ICacheProvider<ITestInterface>>();
+            var cacheProvider = new Mock<ICacheProvider<ITestInterface>>(MockBehavior.Strict);
             var policyProvider = new Mock<IPolicyProvider<ITestInterface>>(MockBehavior.Strict);
 
             var expectedResult = new[] { 1, 2, 3 };
@@ -48,10 +67,11 @@ namespace DR.Sleipner.Caching.Test
             var cachedObject = new CachedObject<IEnumerable<int>>(CachedObjectState.Stale, expectedResult);
             var call = ExpressionHelpers.GetMethodCallDescription<ITestInterface, IEnumerable<int>>(a => a.ParameteredMethod("a", 22));
 
-            testInterface.Setup(a => a.ParameteredMethod("a", 22)).Returns(newResult);
-
             var lookupContext = new DefaultLookupContextBuilder<ITestInterface>().CreateLookupContext<IEnumerable<int>>(testInterface.Object, cacheProvider.Object, policyProvider.Object, call.Method, call.Parameters);
             lookupContext.CurrentCachedObject = cachedObject;
+
+            testInterface.Setup(a => a.ParameteredMethod("a", 22)).Returns(newResult);
+            cacheProvider.Setup(a => a.StoreItem(lookupContext, newResult));
 
             var asyncHandler = new AsyncronousLookupHandler<ITestInterface>();
             var actualResult = asyncHandler.Handle(lookupContext);
@@ -67,7 +87,7 @@ namespace DR.Sleipner.Caching.Test
         public void HandleTest_Stale_Exception()
         {
             var testInterface = new Mock<ITestInterface>(MockBehavior.Strict);
-            var cacheProvider = new Mock<ICacheProvider<ITestInterface>>();
+            var cacheProvider = new Mock<ICacheProvider<ITestInterface>>(MockBehavior.Strict);
             var policyProvider = new Mock<IPolicyProvider<ITestInterface>>(MockBehavior.Strict);
 
             var expectedResult = new[] { 1, 2, 3 };
@@ -75,10 +95,11 @@ namespace DR.Sleipner.Caching.Test
             var cachedObject = new CachedObject<IEnumerable<int>>(CachedObjectState.Stale, expectedResult);
             var call = ExpressionHelpers.GetMethodCallDescription<ITestInterface, IEnumerable<int>>(a => a.ParameteredMethod("a", 22));
 
-            testInterface.Setup(a => a.ParameteredMethod("a", 22)).Throws(exception);
-
             var lookupContext = new DefaultLookupContextBuilder<ITestInterface>().CreateLookupContext<IEnumerable<int>>(testInterface.Object, cacheProvider.Object, policyProvider.Object, call.Method, call.Parameters);
             lookupContext.CurrentCachedObject = cachedObject;
+
+            testInterface.Setup(a => a.ParameteredMethod("a", 22)).Throws(exception);
+            cacheProvider.Setup(a => a.StoreException(lookupContext, exception));
 
             var asyncHandler = new AsyncronousLookupHandler<ITestInterface>();
             var actualResult = asyncHandler.Handle(lookupContext);
@@ -93,13 +114,64 @@ namespace DR.Sleipner.Caching.Test
         [Test]
         public void HandleTest_None()
         {
-            Assert.Fail("Not implemented");
+            var testInterface = new Mock<ITestInterface>(MockBehavior.Strict);
+            var cacheProvider = new Mock<ICacheProvider<ITestInterface>>(MockBehavior.Strict);
+            var policyProvider = new Mock<IPolicyProvider<ITestInterface>>(MockBehavior.Strict);
+
+            var newResult = new[] { 1, 2, 3 };
+            var cachedObject = new CachedObject<IEnumerable<int>>(CachedObjectState.None, default(IEnumerable<int>));
+            var call = ExpressionHelpers.GetMethodCallDescription<ITestInterface, IEnumerable<int>>(a => a.ParameteredMethod("a", 22));
+
+            var lookupContext = new DefaultLookupContextBuilder<ITestInterface>().CreateLookupContext<IEnumerable<int>>(testInterface.Object, cacheProvider.Object, policyProvider.Object, call.Method, call.Parameters);
+            lookupContext.CurrentCachedObject = cachedObject;
+
+            testInterface.Setup(a => a.ParameteredMethod("a", 22)).Returns(newResult);
+            cacheProvider.Setup(a => a.StoreItem(lookupContext, newResult));
+
+            var asyncHandler = new AsyncronousLookupHandler<ITestInterface>();
+            var actualResult = asyncHandler.Handle(lookupContext);
+            Assert.AreSame(newResult, actualResult);
+
+            testInterface.Verify(a => a.ParameteredMethod("a", 22), Times.Once());
+            cacheProvider.Verify(a => a.StoreItem(lookupContext, newResult), Times.Once());
+            cacheProvider.Verify(a => a.StoreException(lookupContext, It.IsAny<Exception>()), Times.Never());
         }
 
         [Test]
         public void HandleTest_None_Exception()
         {
-            Assert.Fail("Not implemented");
+            var testInterface = new Mock<ITestInterface>(MockBehavior.Strict);
+            var cacheProvider = new Mock<ICacheProvider<ITestInterface>>(MockBehavior.Strict);
+            var policyProvider = new Mock<IPolicyProvider<ITestInterface>>(MockBehavior.Strict);
+
+            var exception = new TestException();
+            var cachedObject = new CachedObject<string>(CachedObjectState.None, default(string));
+            var call = ExpressionHelpers.GetMethodCallDescription<ITestInterface, string>(a => a.ParameterLessMethod());
+
+            var lookupContext = new DefaultLookupContextBuilder<ITestInterface>().CreateLookupContext<string>(testInterface.Object, cacheProvider.Object, policyProvider.Object, call.Method, call.Parameters);
+            lookupContext.CurrentCachedObject = cachedObject;
+
+            testInterface.Setup(a => a.ParameterLessMethod()).Throws(exception);
+            cacheProvider.Setup(a => a.StoreException(lookupContext, exception));
+
+            var asyncHandler = new AsyncronousLookupHandler<ITestInterface>();
+            try
+            {
+                asyncHandler.Handle(lookupContext);
+                Assert.Fail("Handle did not throw exception");
+            }
+            catch(TestException) { /*this is expected. Just go on*/ }
+            catch
+            {
+                Assert.Fail("Handle did not throw proper exception");
+            }
+
+            testInterface.Verify(a => a.ParameterLessMethod(), Times.Once());
+            cacheProvider.Verify(a => a.StoreException(lookupContext, exception), Times.Once());
         }
+    }
+
+    public class TestException : Exception
+    {
     }
 }
