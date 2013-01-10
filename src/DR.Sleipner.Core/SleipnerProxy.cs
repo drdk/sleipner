ff9e9f75a4e944e2dc038d95d640b9f62a4ee447
Jonas Swiatek <jals@dr.dk>
2013-01-10 17:10:34 +0100

    Added docs

--- src/DR.Sleipner.Core/SleipnerProxy.cs
+++ src/DR.Sleipner.Core/SleipnerProxy.cs
@@ -8,13 +8,13 @@ namespace DR.Sleipner.Core
 {
     public class SleipnerProxy<T> where T : class
     {
-        private readonly T _realInstance;
+        protected readonly T RealInstance;
         private readonly IInterceptHandler<T> _interceptHandler;
         public T Object;
 
         public SleipnerProxy(T realInstance, IInterceptHandler<T> interceptHandler)
         {
-            _realInstance = realInstance;
+            RealInstance = realInstance;
             _interceptHandler = interceptHandler;
 
             Object = ProxyGeneratorCache.GetProxy(realInstance, interceptHandler);
